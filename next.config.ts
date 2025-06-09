1eb9215dbe69a7ecf2322c9c78bf55bd4fbd2dc6
Muhammad Umer <rawalaumer@gmail.com>
2025-06-09 17:30:32 +0500

    theme & header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Disables ESLint checks during `next build`
+  },
+  // ...any other config options
 };
 
 export default nextConfig;
